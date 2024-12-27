b18f7e7d4aecda6e9f3ae1ece65babf9b293c620
Khaled Hasan <126507374+1ittleBee@users.noreply.github.com>
2024-12-27 19:38:32 +0600

    Update 3-deploy_migration.js

--- src/migrations/3-deploy_migration.js
+++ src/migrations/3-deploy_migration.js
@@ -1,4 +1,12 @@
-const SimpleStorage = artifacts.require("record");
-module.exports = function (deployer) {
-  deployer.deploy(SimpleStorage);
+const Record = artifacts.require("record");
+
+module.exports = async function (deployer) {
+  try {
+    await deployer.deploy(Record);
+    const instance = await Record.deployed();
+    console.log("Record contract deployed at:", instance.address);
+  } catch (error) {
+    console.error("Error deploying Record contract:", error);
+    throw error;
+  }
 };
